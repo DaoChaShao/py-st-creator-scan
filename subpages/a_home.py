c3964999441d4c1a3acc7194e2a6d364c76870ca
DaoChaShao <27882272@qq.com>
2025-05-05 22:54:40 +0800

    fix: update application title and enhance the introduction details in a_home.py

--- subpages/a_home.py
+++ subpages/a_home.py
@@ -8,11 +8,15 @@
 
 from streamlit import title, divider, expander, caption, empty
 
-title("Tiktok Short Video Analysis")
+title("Short Video Analysis")
 divider()
-with expander("Introduction", expanded=True):
-    caption("1. 抖音账号：@章职电商·青春纪")
-    caption("2. 数据范围：2025年4月2日至2025年5月1日")
+with expander("APPLICATION INTRODUCTION", expanded=True):
+    caption("1. Arrangement of Data：from2nd April 2025 to 1st May 2025")
+    caption("2. Categories of Data：date, time, like, comment, collection, forward, face, duration, content")
+    caption("3. Data Source：CSV file, UTF-8 encoding")
+    caption("4. Data Cleaning：Remove duplicate data, handle missing values, convert data types")
+    caption("5. Data Visualization：Use Streamlit to visualize data")
+    caption("6. Data Analysis：Use Python to analyze data")
 
 empty_message = empty()
 empty_message.info("Please check the details at the different pages of manipulation.")
